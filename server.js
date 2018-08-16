70d17c39b056528ba09493ca58054cd207f7fc13
Nadezhda Bremmekamp <nadezhda.bremmekamp@hwb-ag.local>
2018-08-16 17:31:33 +0200

    Setup startscript and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 5000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -52,6 +53,7 @@ app.get('/bad',(req, res) => {
     errorMessage: 'Bad request'
   });
 })
-app.listen(5000, () => {
-  console.log('Server is up on port 5000');
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
 });
